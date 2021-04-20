95ab6ec7990339d9bbc5e3e1648646d102ae1e4d
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-04-20 10:12:46 +0100

    (improvement) added logic for incrementing counts

--- app/src/main/java/com/android/hellozuri/MainActivity.kt
+++ app/src/main/java/com/android/hellozuri/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.android.hellozuri
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.hellozuri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleButtonClick()
+    }
+
+    /**
+     *   Separate function for listening to Button clicks
+     */
+    private fun handleButtonClick() {
+        binding.incrementCountBtn.setOnClickListener {
+            incrementCount()
+        }
+    }
+
+    /**
+     *  Increments the counter by 1 and set the text to the TextView
+     */
+    private fun incrementCount() {
+        count += 1
+        binding.showCountsTextView.text = count.toString()
     }
 }
